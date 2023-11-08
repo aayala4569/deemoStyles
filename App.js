136311383b5d71036212746c977f4a3d946081ac
aayala4569 <angelicaayala100@gmail.com>
2023-11-07 19:31:45 -0800

    our first commit for demo styles

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import AppText from "./components/AppText";
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <MaterialCommunityIcons name="email" size={150} color="hotpink" />
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "skyblue",
+          borderRadius: 50,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "orange",
+          width: 100,
+          height: 100,
+          shadowColor: "gray",
+          shadowOffset: { width: 10, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+          elevation: 40,
+        }}
+      />
+      <AppText
+        style={{
+          fontSize: 40,
+          fontFamily: "Chalkboard SE",
+          fontWeight: 600,
+          color: "tomato",
+          textTransform: "capitalize",
+          textDecorationLine: "underline line-through",
+          textAlign: "center",
+          lineHeight: 40
+        }}
+      >
+        I love React Native! My first react native app. Here is some more text.
+      </AppText>
+      <View
+        style={{
+          backgroundColor: "skyblue",
+          width: 100,
+          height: 100,
+          paddingRight: 25,
+        }}
+      >
+        <View style={{ backgroundColor: "gold", height: 50, width: 50 }} />
+      </View>
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+          margin: 20,
+        }}
+      ></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
